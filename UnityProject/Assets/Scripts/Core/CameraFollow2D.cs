b96260a9eb586c16e59b0a2197230a5ec6c63dfb
Resul <63105852+shironaki@users.noreply.github.com>
2026-09-03 11:39:50 +0300

    Fix camera bounds configuration for dungeon follow

--- UnityProject/Assets/Scripts/Core/CameraFollow2D.cs
+++ UnityProject/Assets/Scripts/Core/CameraFollow2D.cs
@@ -6,12 +6,17 @@ namespace ShadowAscension.Core
     {
         [SerializeField] private Transform target;
         [SerializeField] private float smoothTime = 0.12f;
-        [SerializeField] private Vector2 minBounds = new Vector2(0f, 0.5f);
-        [SerializeField] private Vector2 maxBounds = new Vector2(0f, 0.5f);
+        [SerializeField] private Vector2 minBounds = new Vector2(-2f, -1.5f);
+        [SerializeField] private Vector2 maxBounds = new Vector2(2f, 1.5f);
 
         private Vector3 velocity;
 
         public void SetTarget(Transform newTarget) => target = newTarget;
+        public void SetBounds(Vector2 minimum, Vector2 maximum)
+        {
+            minBounds = new Vector2(Mathf.Min(minimum.x, maximum.x), Mathf.Min(minimum.y, maximum.y));
+            maxBounds = new Vector2(Mathf.Max(minimum.x, maximum.x), Mathf.Max(minimum.y, maximum.y));
+        }
 
         private void LateUpdate()
         {
